7428f57afc440c49bf266567db5e92c7e94ba77d
josh <io@ioio.nu>
2020-09-13 14:54:47 +1000

    stub migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from import_export import resources
+from blog.models import Post
 
 # Register your models here.
 from .models import Post, Image
 
 admin.site.register(Post)
 admin.site.register(Image)
+
+class PostResource(resources.ModelResource):
+  class Meta:
+    model = Post
